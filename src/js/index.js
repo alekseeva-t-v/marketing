26b68a49f018d27a2523480a24b2f3c0b51afadc
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-10-02 05:09:56 +0300

    feat: add section Portfolio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import mixitup from 'mixitup';
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
+$(function () {
+  var mixer = mixitup('.portfolio__content');
+  Fancybox.bind('[data-fancybox]', {});
+});

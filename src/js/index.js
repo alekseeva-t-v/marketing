8a15d1f908233476cbe91f2de975b8dcf8a4d748
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-10-06 15:01:32 +0300

    feat: add section Slider-blog

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,10 @@ import '@fancyapps/ui/dist/fancybox/fancybox.css';
 $(function () {
   var mixer = mixitup('.portfolio__content');
   Fancybox.bind('[data-fancybox]', {});
+
+  $('.slider-blog__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg"></button>',
+  });
 });

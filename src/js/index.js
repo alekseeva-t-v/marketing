11f3f0075ea24f93c12e9ba043e775cea9aef538
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-10-13 08:58:56 +0300

    feat: add adaptive Header block

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,19 @@ import { Fancybox } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 $(function () {
-  var mixer = mixitup('.portfolio__content');
   Fancybox.bind('[data-fancybox]', {});
 
   $('.slider-blog__inner').slick({
     dots: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg"></button>',
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="./img/arrow-right.svg"></button>',
   });
+
+  $('.menu__btn').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+  var mixer = mixitup('.portfolio__content');
 });

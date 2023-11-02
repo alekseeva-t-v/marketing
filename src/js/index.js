80e58494692e8b4183f4e040ee0eae883f5c779c
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-11-02 14:05:31 +0300

    fix: change the text on the site

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,13 @@ import { Fancybox } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 $(function () {
+  $('.menu a, .go-top, .header__icon').on('click', function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
   Fancybox.bind('[data-fancybox]', {});
 
   $('.slider-blog__inner').slick({
@@ -21,8 +28,12 @@ $(function () {
     ]
   });
 
-  $('.menu__btn').on('click', function () {
+  $('.menu__btn, .menu a').on('click', function () {
     $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active-logo');
   });
 
   var mixer = mixitup('.portfolio__content');
